e33ce42c54b119cfd12941b4ee083411834199b6
Wulan <widyawulangit19@gmail.com>
2026-02-15 23:07:25 +0700

    perubahan navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import '../styles/components/Navbar.css'
 
 const Navbar = ()=> {
   const [scrolled, setScrolled] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -17,11 +18,27 @@ const Navbar = ()=> {
     <nav className={`navbar ${scrolled ? "scrolled" : ""}`}>
       <div className="nav-container">
         <div className="logo">LADZZ BOUTIQUE</div>
+
         <div className="nav-links">
           <a href="#about">About</a>
           <a href="#collection">Collection</a>
           <a href="#investment">Lookbook</a>
-          <a href="#" className="nav-cta">Shop</a>
+          <a  href="https://wa.me/message/6663KZLPCMINH1"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="nav-cta">
+              Shop
+          </a>
+        </div>
+
+        {/* Hamburger */}
+        <div 
+          className={`hamburger ${menuOpen ? "open" : ""}`} 
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
         </div>
       </div>
     </nav>
